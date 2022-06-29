5b94192b766af127d415f7658b72f94ec6da35c4
yudiz_vajid <vajid.k@yudiz.in>
2022-06-29 15:34:26 +0530

    USV-46

--- src/app/tableManager/player/service.ts
+++ src/app/tableManager/player/service.ts
@@ -153,7 +153,7 @@ class Service {
    * get list of cards for user to play wrt discard pile top card
    */
   public async getPlayableCardIds(oDiscardPileTopCard: ICard, eNextCardColor?: Table['eNextCardColor']) {
-    if (oDiscardPileTopCard.nLabel === 12) return this.aHand.filter(card => card.nLabel === 12||oDiscardPileTopCard.eColor=== card.eColor).map(card => card.iCardId); // TODO check color as well
+    if (oDiscardPileTopCard.nLabel === 12) return this.aHand.filter(card => card.nLabel > 12 || card.nLabel === 12||oDiscardPileTopCard.eColor=== card.eColor).map(card => card.iCardId); // TODO check color as well
     if (oDiscardPileTopCard.nLabel === 14) return this.aHand.filter(card => card.nLabel === 14||oDiscardPileTopCard.eColor=== card.eColor).map(card => card.iCardId);
     if (oDiscardPileTopCard.nLabel === 13) return this.aHand.filter(card => card.nLabel > 12 || card.eColor === eNextCardColor).map(card => card.iCardId);
     return this.aHand
