6535e87c0294555418dbe83078de55d1f27e28f4
yudiz_vajid <vajid.k@yudiz.in>
2022-06-29 17:28:04 +0530

    USV-46

--- dist/app/tableManager/player/service.js
+++ dist/app/tableManager/player/service.js
@@ -128,7 +128,7 @@ class Service {
             if (oDiscardPileTopCard.nLabel === 12)
                 return this.aHand.filter(card => card.nLabel > 12 || card.nLabel === 12 || oDiscardPileTopCard.eColor === card.eColor).map(card => card.iCardId);
             if (oDiscardPileTopCard.nLabel === 14)
-                return this.aHand.filter(card => card.nLabel === 14 || oDiscardPileTopCard.eColor === card.eColor).map(card => card.iCardId);
+                return this.aHand.filter(card => card.nLabel > 12 || card.nLabel === 14 || oDiscardPileTopCard.eColor === card.eColor).map(card => card.iCardId);
             if (oDiscardPileTopCard.nLabel === 13)
                 return this.aHand.filter(card => card.nLabel > 12 || card.eColor === oDiscardPileTopCard.eColor).map(card => card.iCardId);
             return this.aHand
--- src/app/tableManager/player/service.ts
+++ src/app/tableManager/player/service.ts
@@ -154,7 +154,7 @@ class Service {
    */
   public async getPlayableCardIds(oDiscardPileTopCard: ICard, eNextCardColor?: Table['eNextCardColor']) {
     if (oDiscardPileTopCard.nLabel === 12) return this.aHand.filter(card => card.nLabel > 12 || card.nLabel === 12||oDiscardPileTopCard.eColor=== card.eColor).map(card => card.iCardId); // TODO check color as well
-    if (oDiscardPileTopCard.nLabel === 14) return this.aHand.filter(card => card.nLabel === 14||oDiscardPileTopCard.eColor=== card.eColor).map(card => card.iCardId);
+    if (oDiscardPileTopCard.nLabel === 14) return this.aHand.filter(card => card.nLabel > 12 || card.nLabel === 14||oDiscardPileTopCard.eColor=== card.eColor).map(card => card.iCardId);
     if (oDiscardPileTopCard.nLabel === 13) return this.aHand.filter(card => card.nLabel > 12 || card.eColor === oDiscardPileTopCard.eColor).map(card => card.iCardId);
     return this.aHand
       .filter(card => oDiscardPileTopCard.eColor === card.eColor || oDiscardPileTopCard.nLabel === card.nLabel || card.nLabel === 13 || card.nLabel === 14)
