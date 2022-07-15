2d1b0a8d66d1ac8198fa3af9d45d7b1c89896732
yudiz_vajid <vajid.k@yudiz.in>
2022-07-15 16:18:10 +0530

    USV-78

--- dist/app/tableManager/player/index.js
+++ dist/app/tableManager/player/index.js
@@ -54,7 +54,10 @@ class Player extends service_1.default {
             }
             else {
                 const iNextPlayerId = yield oTable.getNextPlayer(this.nSeat);
-                aPromises.push(oTable.update({ nDrawCount: oCardToDiscard.nLabel === 13 ? 1 : (4 + (oTable.toJSON().nDrawCount === 1 ? 0 : oTable.toJSON().nDrawCount)), iDrawPenltyPlayerId: iNextPlayerId === null || iNextPlayerId === void 0 ? void 0 : iNextPlayerId.iPlayerId }));
+                aPromises.push(oTable.update({
+                    nDrawCount: oCardToDiscard.nLabel === 13 ? 1 : (4 + (oTable.toJSON().nDrawCount === 1 ? 0 : oTable.toJSON().nDrawCount)),
+                    iDrawPenltyPlayerId: oCardToDiscard.nLabel === 13 ? '' : iNextPlayerId === null || iNextPlayerId === void 0 ? void 0 : iNextPlayerId.iPlayerId
+                }));
             }
             aPromises.push(oTable.update({ iPlayerTurn: "" }));
             aPromises.push(oTable.addToDiscardPile(oCardToDiscard));
