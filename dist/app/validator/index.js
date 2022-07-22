434de3aea536d3f5b24111ccc324bd526f0aae2e
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2022-07-22 15:18:09 +0530

    (WIP) grpc

--- dist/app/validator/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.verifySettings = exports.verifyAuthHeader = void 0;
-var verifyAuthHeaders_1 = require("./lib/verifyAuthHeaders");
-Object.defineProperty(exports, "verifyAuthHeader", { enumerable: true, get: function () { return verifyAuthHeaders_1.isValidRequest; } });
-var verifySettings_1 = require("./lib/verifySettings");
-Object.defineProperty(exports, "verifySettings", { enumerable: true, get: function () { return verifySettings_1.isValidRequest; } });
--- dist/grpc/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const protos = [
-    {
-        path: `${__dirname}/protos/AuthService.proto`,
-        name: 'AuthService',
-    },
-];
-exports.default = protos;
