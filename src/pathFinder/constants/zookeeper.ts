011c859a7566da87958d52387b3a7462d0a4c83e
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2022-07-26 17:47:37 +0530

    (WIP) grpc

--- src/pathFinder/constants/zookeeper.ts
+++ src/pathFinder/constants/zookeeper.ts
@@ -1,13 +1,13 @@
 const exportObject = Object.freeze({
-  DB_CONFIG: 'config',
-  GAME_CONFIG: 'game-config',
+  // DB_CONFIG: 'config',
+  // GAME_CONFIG: 'game-config',
   IPs: 'zk1.mpl.internal:2181,zk2.mpl.internal:2181,zk3.mpl.internal:2181',
-  SERVER_CONFIG_PATH: '/mpl/phase10/config',
-  DB_CONFIG_PATH: '/mpl/callbreak-v4/DBConfig',
-  GAME_CONFIG_PATH: '/mpl/callbreak-v4/GameConfig',
-  MSG_CONFIG_PATH: '/mpl/callbreak-v4/MsgConfig',
-  ERRORS: {
-    WTIARAE: 'This game has ended due to inactivity.'
-  }
+  SERVER_CONFIG_PATH: '/mpl/callbreak-v4/DBConfig',
+//   DB_CONFIG_PATH: '/mpl/callbreak-v4/DBConfig',
+//   GAME_CONFIG_PATH: '/mpl/callbreak-v4/GameConfig',
+//   MSG_CONFIG_PATH: '/mpl/callbreak-v4/MsgConfig',
+//   ERRORS: {
+//     WTIARAE: 'This game has ended due to inactivity.'
+//   }
 });
 export = exportObject;
