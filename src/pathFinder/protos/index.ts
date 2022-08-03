81333d3a6e63216bead347675ed860d3f414f6bc
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2022-08-03 08:20:58 +0530

    USV-2

--- src/pathFinder/protos/index.ts
+++ src/pathFinder/protos/index.ts
@@ -3,10 +3,10 @@ const protos = [
     path: `${__dirname}/lib/AuthService.proto`,
     name: 'service-auth',
   },
-  // {
-  //   path: `${__dirname}/lib/LobbyService.proto`,
-  //   name: 'service-tournament-1v1',
-  // },
+  {
+    path: `${__dirname}/lib/LobbyService.proto`,
+    name: 'service-tournament-1v1',
+  },
 ];
 
 export default protos;
